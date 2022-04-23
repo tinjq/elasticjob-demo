a52c581633b794296386c9b52c250eea5b9d8020
tinjq <1967694532@qq.com>
2022-04-23 11:05:52 +0800

    first commit

---
+++ src/main/java/com/jqtin/springdemo/jobs/MyJob.java
@@ -0,0 +1,17 @@
+package com.jqtin.springdemo.jobs;
+
+import java.time.LocalDateTime;
+
+import org.apache.shardingsphere.elasticjob.api.ShardingContext;
+import org.apache.shardingsphere.elasticjob.simple.job.SimpleJob;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyJob implements SimpleJob {
+
+    @Override
+    public void execute(ShardingContext arg0) {
+        System.out.println(LocalDateTime.now().toString() + " job test.");
+    }
+    
+}
